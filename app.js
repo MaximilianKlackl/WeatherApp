f4d5c2712b39d994e92153c43e16375ebbc4ab02
MaximilianKlackl <46849661+MaximilianKlackl@users.noreply.github.com>
2019-08-25 00:54:08 +0200

    hi

--- app.js
+++ app.js
@@ -1,68 +1,50 @@
 //l1na2zqf.ygp@20minutemail.it
 
-setDefaultTempType();
-
-function setDefaultTempType()
-{
-    if(localStorage.getItem("tempType") == null)
-    {
-        localStorage.setItem("tempType", "true");
-    }
-}
-
 document.getElementById("celcius").addEventListener("click", function()
 {
-    if(localStorage.getItem("tempType") == "false")
+    //styling
+    document.getElementById("celcius").style.color = "#F67280";
+    document.getElementById("fahrenheit").style.color = "#AAAAAA";
+
+    let celcius = document.getElementById("degree").innerHTML; 
+
+    document.getElementById("degree").innerHTML = round(convert(celcius));
+
+    //FORECAST:
+    //change °C to °F
+    let containerForcast = document.getElementById("container-forecast");
+    let containerItemsForcast = containerForcast.children;
+
+    for(let i = 0; i < containerItemsForcast.length; i++)
     {
-        console.log("1");
-        document.getElementById("celcius").style.color = "#F67280";
-        document.getElementById("fahrenheit").style.color = "#AAAAAA";
-
-        localStorage.removeItem("tempType");
-        localStorage.setItem("tempType", "true");
-        let beforeTemp = document.getElementById("degree").innerHTML; 
-        let slicedTemp = beforeTemp.substring(0, beforeTemp.length-2);
-        document.getElementById("degree").innerHTML = Math.round(convertTemp(slicedTemp) * 100) / 100 + " °C";
-
-        let containerForcast = document.getElementById("container-forecast");
-        let containerItemsForcast = containerForcast.children;
-
-        for(let i = 0; i < containerItemsForcast.length; i++)
-        {
-            let children = containerItemsForcast[i].children;
-            let beforeTemp = children[2].innerHTML;
-            let slicedTemp = beforeTemp.substring(0, beforeTemp.length-2);
-            children[2].innerHTML = Math.round(convertTemp(slicedTemp) * 100) / 100 + " °C";
-        }
+        let children = containerItemsForcast[i].children;
+        let celcius2 = children[2].innerHTML;
+        children[2].innerHTML = round(convert(celcius2));
     }
+
 }) 
 
 document.getElementById("fahrenheit").addEventListener("click", function(){
 
-    if(localStorage.getItem("tempType") === "true")
+    
+    //styling
+    document.getElementById("fahrenheit").style.color = "#F67280";
+    document.getElementById("celcius").style.color = "#AAAAAA";
+
+    //change °C to °F
+    let fahrenheit = document.getElementById("degree").innerHTML;  
+    document.getElementById("degree").innerHTML =  round(convert(fahrenheit));
+
+    let containerForcast = document.getElementById("container-forecast");
+    let containerItemsForcast = containerForcast.children;
+
+    for(let i = 0; i < containerItemsForcast.length; i++)
     {
-        console.log("2");
-
-        document.getElementById("fahrenheit").style.color = "#F67280";
-        document.getElementById("celcius").style.color = "#AAAAAA";
-
-        localStorage.removeItem("tempType");
-        localStorage.setItem("tempType", "false");
-        let beforeTemp = document.getElementById("degree").innerHTML; 
-        let slicedTemp = beforeTemp.substring(0, beforeTemp.length-2);
-        document.getElementById("degree").innerHTML = Math.round(convertTemp(slicedTemp) * 100) / 100  + " °F";
-
-        let containerForcast = document.getElementById("container-forecast");
-        let containerItemsForcast = containerForcast.children;
-
-        for(let i = 0; i < containerItemsForcast.length; i++)
-        {
-            let children = containerItemsForcast[i].children;
-            let beforeTemp = children[2].innerHTML;
-            let slicedTemp = beforeTemp.substring(0, beforeTemp.length-2);
-            children[2].innerHTML = Math.round(convertTemp(slicedTemp) * 100) / 100 + " °F";
-        }
+        let children = containerItemsForcast[i].children;
+        let fahrenheit2 = children[2].innerHTML;
+        children[2].innerHTML =  round(convert(fahrenheit2));
     }
+    
 }) 
 
 window.addEventListener("load", ()=> {
@@ -95,13 +77,14 @@ window.addEventListener("load", ()=> {
 
                     console.log(data)
 
-                    const {temperature, summary, icon} = data.currently;
+                    const {summary, icon} = data.currently;
                     const daily = data.daily.data;
 
+                    let temperature = data.currently.temperature + " °F";
                     console.log(daily);
 
                     //set DOM Elements 
-                    document.getElementById("degree").innerHTML = Math.round((convertTemp(temperature)) * 100) / 100 + getTempSign();
+                    document.getElementById("degree").innerHTML =  round(convert(temperature));
                     document.getElementById("location").innerHTML = city;
                     document.getElementById("summary").innerHTML = summary;
 
@@ -113,35 +96,23 @@ window.addEventListener("load", ()=> {
                     {
                         let children = containerItemsForcast[i].children;
                         children[0].innerHTML = convertTime(daily[i].time);
-                        setIcons(daily[i].icon, children[1]);
-                        children[2].innerHTML =  Math.round((convertTemp((daily[i].temperatureLow + daily[i].temperatureHigh) / 2)) * 100) / 100 + getTempSign();
+                        setIcons(daily[i].icon, children[1], "#333333");
+                        children[2].innerHTML =  round(convert(((daily[i].temperatureLow + daily[i].temperatureHigh)/2 + " °F")));
                         children[3].innerHTML = daily[i].summary;
                     }
 
                     //set today icon
-                    setIcons(icon, document.querySelector("#icon"));
+                    setIcons(icon, document.querySelector("#icon"), "white");
                 })
         })
 })
 
-function setIcons(icon, iconID)
+function setIcons(icon, iconID, colorID)
 {
-    const skycons = new Skycons({ color: "white" });
+    const skycons = new Skycons({ color: colorID });
     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
     skycons.play();
     return skycons.set(iconID, Skycons[currentIcon]);
-}
-
-function convertTemp(degree)
-{
-    if(localStorage.getItem("tempType") == "true")
-    {
-        return (degree - 32) * (5/9);
-    }
-    else
-    {
-        return (degree * (9/5)) + 32;
-    }
 }
 
 function convertTime(unixTime){
@@ -155,9 +126,6 @@ function convertTime(unixTime){
     // Convert timestamp to milliseconds
     var date = new Date(unixtimestamp*1000);
    
-    // Year
-    var year = date.getFullYear();
-   
     // Month
     var month = months_arr[date.getMonth()];
    
@@ -165,19 +133,30 @@ function convertTime(unixTime){
     var day = date.getDate();
    
     // Display date time in MM-dd-yyyy h:m:s format
-    var convdataTime = month+' '+day+'th '+year;
+    var convdataTime = month+' '+day+'th';
     
     return convdataTime;
 }
 
-function getTempSign()
+function convert(temp)
 {
-    if(localStorage.getItem("tempType") == "true")
+    temp += "";
+    let sign = temp.substring(temp.length, temp.length -3);
+    let degree = temp.substring(0, temp.length-3);
+
+    if(sign == " °F")
     {
-        return " °C";
+        return  (degree - 32) * (5/9) + " °C";
     }
-    else
+    if(sign == " °C")
     {
-        return " °F";
+        return (degree * (9/5)) + 32 + " °F";
     }
+}
+
+function round(num)
+{
+    let sign = num.substring(num.length, num.length -3);
+    degree = num.substring(0, num.length-3);
+    return Math.round(degree * 100) / 100 + sign;
 }
